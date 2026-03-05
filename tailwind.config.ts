cb656aa65edea6aa5158f2350ee825a17d63232f
alex banele <alexsouthflow@gmail.com>
2026-03-05 07:43:07 +0200

    Fix tailwind build error and add missing typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: ['class'],
@@ -106,7 +108,7 @@ const config: Config = {
       }
     }
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [tailwindAnimate, typography],
 };
 
 export default config;
